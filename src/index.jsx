3be2470622f77cdce2a624087bb50b999fa9c44b
Victor Martinez <knoopx@gmail.com>
2022-04-03 21:45:29 +0200

    fix: react 18 deprecations

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,8 @@
 import React from "react"
-import ReactDOM from "react-dom"
+import { createRoot } from "react-dom/client"
 
 import "./index.css"
 import App from "./components/App"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root"),
-)
+const root = createRoot(document.getElementById("root"))
+root.render(<App />)
